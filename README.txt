298bee66b88a79e6da28d3a1c3874890d6ae29bc
Roniel <ronielabreu22@gmail.com>
2020-11-12 18:16:33 -0500

    files for the hw

---
+++ README.txt
@@ -0,0 +1,12 @@
+Link to github pages = https://paradox05.github.io/HW5/
+Link to repository = https://github.com/Paradox05/HW5
+
+HW6
+Name: Roniel Abreu, 
+email: roniel_abreu@student.uml.edu
+Affiliation: I am a senior majoring in computer science and this assignment is for 
+Graphical User Interface programming I with wenjin Zhou
+Date:11/12/20
+Description: We first make a simple html layout using form and the main goal of this 
+project is to use javascript to be able to make a table dynamically
+then we use jquery to validate all data entry 
---
+++ js/main.js
@@ -0,0 +1,92 @@
+//This function is for some tests i run
+var debugging = true;
+$(document).ready(function() {
+
+    var i = 0;
+    var inputs = [1,2,3,4];
+    $('input').each(function() {
+        $(this).val(inputs[i]);
+        i++;
+    }) 
+
+    $form = $('form');
+    $form.validate({
+        highlight: function(element) {
+            $(element).removeClass('input-valid').addClass('input-error');
+        },
+
+        unhighlight: function(element) {
+            $(element).removeClass('input-error').addClass('input-valid');
+        }
+    });
+
+
+    $('input').each(function() {
+        $(this).rules('add', {
+            required: true,
+            range: [-50, 50]
+        });
+    });
+
+    $('button').on('click',function(e) {
+        e.preventDefault();
+        if ($form.valid())
+            generate();
+    })
+});
+
+
+
+
+
+// this is our generate function where we generate the table
+function generate() {
+    
+    var array = [],i,j;
+
+    $('input').each(function() {
+        array.push(parseInt($(this).val(),10));
+    });
+
+
+    for(i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+    //swap if needed
+    for(var i =0; i < array.length;i++) {
+        if(array[i] > array[i+1]) {
+            [array[i+1], array[i]] = [array[i], array[i+1]];
+        }
+    }
+    var $table = $('#tabler')
+    var $col = $table.parent();
+    $table.empty()
+ 
+    var $row = $('<tr>');
+    $row.append('<th>');
+
+    $col.css({
+        'opacity': '0.0',
+        'padding-left': '500px'
+    });
+
+    for(var i = array[0]; i <= array[1]; i++) {
+        $row.append('<th>' + i + '</th>');
+    }
+    $table.append($row);
+    
+    
+    for(var i = array[2]; i <= array[3]; ++i) {
+        $row = $('<tr>');
+
+        $row.append('<th>' + i + '</th>')
+        for(var j = array[0]; j <= array[1]; ++j) {
+            $row.append('<td>' + (i * j) + '</td>');
+        }
+        $table.append($row);
+    }
+    $col.stop(true).animate({
+        opacity: 1.0,       
+    });
+
+}
